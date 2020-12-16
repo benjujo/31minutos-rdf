14f62428b472b5e080664c2ccfd49ab10b78b757
Kyra <kyrantonia@gmail.com>
2020-12-16 11:53:20 -0300

    read files in folder

--- parser.py
+++ parser.py
@@ -1,3 +1,16 @@
 import wikitextparser as wtp
+import os
+
 with open('Articles/Guaripolo.txt', 'r') as file:
     parsed = wtp.parse(file.read())
+    print(parsed.templates[0])
+
+path = 'Articles/'
+##borre las carpetas para no tener problemas
+fileList = os.listdir(path)
+for file in fileList:
+    file = open(path+file, "r")
+    parsed = wtp.parse(file.read())
+    templates= parsed.templates
+    if templates:
+        print(templates[0])
